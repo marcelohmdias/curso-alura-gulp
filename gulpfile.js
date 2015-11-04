abb2fa98c73105eb1eda3f143d708af66e370301
marcelohmdias <marcelohmdias@gmail.com>
2015-11-04 00:02:18 -0200

    Segunda parte, utilizando tasks com dependências.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 'use strict';
 
 var gulp = require( 'gulp' )
-  , imagemin = require( 'gulp-imagemin' );
+  , imagemin = require( 'gulp-imagemin' )
+  , clean = require( 'gulp-clean' );
 
-gulp.task( 'img', function () {
+gulp.task( 'default', [ 'copy' ], function () {
   return gulp.src( 'src/img/**/*' )
     .pipe(imagemin())
     .pipe(gulp.dest( 'dist/img' ));
+});
+
+gulp.task( 'clean', function () {
+  return gulp.src( 'dist' )
+    .pipe(clean());
+});
+
+gulp.task( 'copy', [ 'clean' ], function () {
+  return gulp.src( 'src/**/*' )
+    .pipe(gulp.dest( 'dist' ));
 });
