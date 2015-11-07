d42b2549e407cfb4e37f5401878be1ac94a6f31e
marcelohmdias <marcelohmdias@gmail.com>
2015-11-07 19:27:01 -0200

    Terceira parte, minificação e concatenação de arquivos.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@
 
 var gulp = require( 'gulp' )
   , imagemin = require( 'gulp-imagemin' )
-  , clean = require( 'gulp-clean' );
+  , clean = require( 'gulp-clean' )
+  , concat = require( 'gulp-concat' )
+  , replace = require( 'gulp-html-replace' )
+  , uglify = require( 'gulp-uglify' )
+  , cssmin = require( 'gulp-cssmin' )
+  , usemin = require( 'gulp-usemin' );
 
 gulp.task( 'default', [ 'copy' ], function () {
-  return gulp.src( 'src/img/**/*' )
-    .pipe(imagemin())
-    .pipe(gulp.dest( 'dist/img' ));
+  gulp.start( 'img', 'build' );
 });
 
 gulp.task( 'clean', function () {
@@ -17,5 +20,37 @@ gulp.task( 'clean', function () {
 
 gulp.task( 'copy', [ 'clean' ], function () {
   return gulp.src( 'src/**/*' )
+    .pipe(gulp.dest( 'dist' ));
+});
+
+gulp.task( 'img', function () {
+  gulp.src( 'src/img/**/*' )
+    .pipe(imagemin())
+    .pipe(gulp.dest( 'dist/img' ));
+});
+
+gulp.task( 'build', function () {
+  gulp.src( 'src/**/*.html' )
+    .pipe(usemin({
+        'js': [ uglify ]
+        , 'css': [ cssmin ]
+      }))
+    .pipe(gulp.dest( 'dist' ));
+});
+
+gulp.task( 'js', function () {
+  gulp.src([
+       'src/js/**/jquery.js'
+       , 'src/js/**/home.js'
+       , 'src/js/**/produto.js'
+    ])
+    .pipe(concat( 'all.js' ))
+    .pipe(uglify())
+    .pipe(gulp.dest( 'dist/js/' ));
+});
+
+gulp.task( 'html', function () {
+  gulp.src( 'src/**/*.html' )
+    .pipe(replace({ js: 'js/all.js' }))
     .pipe(gulp.dest( 'dist' ));
 });
